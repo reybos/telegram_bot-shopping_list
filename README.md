0da17e57fa0f0da72d1047b1080a4f86652e6da3
reybos <andreybosiy@yandex.ru>
2024-05-07 11:08:16 +0200

    bot description

--- README.md
+++ README.md
@@ -15,9 +15,29 @@
 
 <h2><a name="about_the_project">About the project</a>&nbsp;&nbsp;<a href="#menu">&#9650;</a></h2>
 <h3><a name="bot_features">Bot Features</a>&nbsp;&nbsp;<a href="#menu">&#9650;</a></h3>
-
 <p>
-    TODO
+This telegram bot provides the ability to keep a shopping list, and its key feature is the ability to combine the list with other users to maintain a common list.
+</p>
+
+- The bot is only available in personal correspondence due to the implementation features.
+- Supports two languages: Russian and English, with the ability to change the command.
+- A user can send a request to another user to join a group and maintain a single list.
+- Confirmation from the list owner is required to join the group.
+- The user who accepted the request is considered the owner of the group
+- In the future, it is planned to add the ability to remove users from the group and block connection requests.
+- Any message sent to the bot will be added to the list
+- Limit on the number of characters per message (30 characters) for normal display in the mobile application.
+- The list is presented as a message with button elements that allow you to delete items from the list.
+- Each member of the group will have one current list, when requesting a new list, the old one is deleted
+- When adding a new entry or deleting an old one, the list is automatically updated both in personal correspondence and among the group members.
+- If the list is empty, the user is prompted to add an entry by sending the bot any message except commands.
+- Basic commands are available in the bot menu, such as displaying the current list, clearing the list, changing the language and requesting another user to join the list.
+
+<p align="center">
+  <b>General view</b>
+</p>
+<p align="center">
+  <img src="img/1.png" height="400" title="general view">
 </p>
 
 <h3><a name="technologies">Technologies</a>&nbsp;&nbsp;<a href="#menu">&#9650;</a></h3>
--- src/main/java/rey/bos/telegram/bot/shopping/list/bot/TelegramBot.java
+++ src/main/java/rey/bos/telegram/bot/shopping/list/bot/TelegramBot.java
@@ -5,10 +5,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+import java.util.List;
 
 @Component
 @Slf4j
@@ -18,25 +25,104 @@ public class TelegramBot implements LongPollingSingleThreadUpdateConsumer {
 
     public TelegramBot(@Value("${telegram.token}") String botToken) {
         telegramClient = new OkHttpTelegramClient(botToken);
+        setCommands();
+    }
+
+    private void setCommands() {
+        SetMyCommands setMyCommands = new SetMyCommands(List.of(
+            new BotCommand("/list", "view the list"),
+            new BotCommand("/clear_list", "clear the list"),
+            new BotCommand("/help", "instruction")
+        ));
+        try {
+            telegramClient.execute(setMyCommands); // Sending our message object to user
+        } catch (TelegramApiException e) {
+            log.error("Can't execute command", e);
+        }
     }
 
     @Override
     public void consume(Update update) {
-        // We check if the update has a message and the message has text
+        log.info(update.getMessage().getFrom().toString());
         if (update.hasMessage() && update.getMessage().hasText()) {
             // Set variables
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
-
-            SendMessage message = SendMessage // Create a message object
-                    .builder()
-                    .chatId(chat_id)
-                    .text(message_text)
-                    .build();
-            try {
-                telegramClient.execute(message); // Sending our message object to user
-            } catch (TelegramApiException e) {
-                log.error("Can't execute command", e);
+            switch (message_text) {
+                case "/start" -> {
+                    String greeting = """
+                        The user's greeting!
+                        Description of the bot's work.
+                        """;
+                    // User send /start
+                    SendMessage message = SendMessage // Create a message object
+                        .builder()
+                        .parseMode("HTML")
+                        .chatId(chat_id)
+                        .text(greeting)
+                        .build();
+                    try {
+                        telegramClient.execute(message); // Sending our message object to user
+                    } catch (TelegramApiException e) {
+                        log.error("Can't execute command", e);
+                    }
+                }
+                case "/list" -> {
+                    SendMessage message = SendMessage.builder()
+                        .parseMode("HTML")
+                        .chatId(chat_id)
+                        .text(
+                            """
+                            <b>Your list</b>
+                            
+                            Click on any item to delete
+                            """
+                        )
+                        .replyMarkup(InlineKeyboardMarkup.builder()
+                            .keyboard(List.of(
+                                new InlineKeyboardRow(
+                                    InlineKeyboardButton
+                                        .builder()
+                                        .text("mandrake root 3 pieces")
+                                        .callbackData("update_msg_text")
+                                        .build()
+                                ),
+                                new InlineKeyboardRow(
+                                    InlineKeyboardButton
+                                        .builder()
+                                        .text("unicorn meat 500g")
+                                        .callbackData("update_msg_text")
+                                        .build()
+                                ),
+                                new InlineKeyboardRow(
+                                    InlineKeyboardButton
+                                        .builder()
+                                        .text("123456789012345678901234567890")
+                                        .callbackData("update_msg_text")
+                                        .build()
+                                )
+                            ))
+                            .build())
+                        .build();
+                    try {
+                        telegramClient.execute(message); // Sending our message object to user
+                    } catch (TelegramApiException e) {
+                        log.error("Can't execute command", e);
+                    }
+                }
+                default -> {
+                    // Unknown command
+                    SendMessage message = SendMessage // Create a message object
+                        .builder()
+                        .chatId(chat_id)
+                        .text("The item has been added to your list")
+                        .build();
+                    try {
+                        telegramClient.execute(message); // Sending our message object to user
+                    } catch (TelegramApiException e) {
+                        log.error("Can't execute command", e);
+                    }
+                }
             }
         }
     }
