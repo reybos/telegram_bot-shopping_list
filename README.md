45dc8e0ce306f321e82b8017dd8cd434d4bd8296
reybos <andreybosiy@yandex.ru>
2024-05-27 18:48:24 +0200

    some ChangeLanguageHandle refactoring and demo video added

--- README.md
+++ README.md
@@ -14,6 +14,9 @@
 </ul>
 
 <h2><a name="about_the_project">About the project</a>&nbsp;&nbsp;<a href="#menu">&#9650;</a></h2>
+<p align="center">
+  <video src='media/demo.mp4' width=600></video>
+</p>
 <h3><a name="bot_features">Bot Features</a>&nbsp;&nbsp;<a href="#menu">&#9650;</a></h3>
 <p>
 This telegram bot provides the ability to keep a shopping list, and its key feature is the ability to combine the list with other users to maintain a common list.
@@ -33,13 +36,6 @@ This telegram bot provides the ability to keep a shopping list, and its key feat
 - When adding a new entry or deleting an old one, the list is automatically updated both in personal correspondence and among the group members.
 - If the list is empty, the user is prompted to add an entry by sending the bot any message except commands.
 - Basic commands are available in the bot menu, such as displaying the current list, clearing the list, changing the language and requesting another user to join the list.
-
-<p align="center">
-  <b>General view</b>
-</p>
-<p align="center">
-  <img src="img/1.png" height="400" title="general view">
-</p>
 
 <h3><a name="technologies">Technologies</a>&nbsp;&nbsp;<a href="#menu">&#9650;</a></h3>
 
@@ -106,6 +102,6 @@ This telegram bot provides the ability to keep a shopping list, and its key feat
 <h3><a name="db_scheme">Database schema</a>&nbsp;&nbsp;<a href="#menu">&#9650;</a></h3>
 
 <p align="center">
-  <img src="img/2.png" height="700" title="general view">
+  <img src="media/2.png" height="700" title="general view">
 </p>
 
--- src/main/java/rey/bos/telegram/bot/shopping/list/bot/handler/impl/callback/ChangeLanguageHandler.java
+++ src/main/java/rey/bos/telegram/bot/shopping/list/bot/handler/impl/callback/ChangeLanguageHandler.java
@@ -3,12 +3,15 @@ package rey.bos.telegram.bot.shopping.list.bot.handler.impl.callback;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import rey.bos.telegram.bot.shopping.list.bot.handler.BotHandler;
 import rey.bos.telegram.bot.shopping.list.io.LanguageCode;
 import rey.bos.telegram.bot.shopping.list.io.entity.User;
 import rey.bos.telegram.bot.shopping.list.service.UserService;
 import rey.bos.telegram.bot.shopping.list.util.BotUtil;
+import rey.bos.telegram.bot.shopping.list.util.MessageUtil;
 
 import static rey.bos.telegram.bot.shopping.list.bot.handler.impl.callback.CallBackCommand.CHANGE_LANGUAGE;
 import static rey.bos.telegram.bot.shopping.list.dictionary.DictionaryKey.CHANGE_LANGUAGE_SUCCESS;
@@ -22,15 +25,19 @@ public class ChangeLanguageHandler extends BotHandler {
 
     private final UserService userService;
     private final BotUtil botUtil;
+    private final MessageUtil messageUtil;
 
     @Override
     public boolean handle(Update update, User user) {
-        String data = update.getCallbackQuery().getData();
+        CallbackQuery query = update.getCallbackQuery();
+        String data = query.getData();
         String languageStr = data.replaceFirst(command.getCommand(), "");
         logCall(user.getId(), command.getCommand(), languageStr);
         LanguageCode language = LanguageCode.valueOf(languageStr);
         user = userService.updateUserLanguage(user.getId(), language);
-        botUtil.sendMessageByKey(user.getTelegramId(), user.getLanguageCode(), CHANGE_LANGUAGE_SUCCESS);
+        int messageId = query.getMessage().getMessageId();
+        EditMessageText message = messageUtil.buildEditMessageText(user, messageId, CHANGE_LANGUAGE_SUCCESS);
+        botUtil.executeMethod(message);
         return true;
     }
 
